a8bca33d733fa84781ebeff456d9464fcb41b55a
Yariv-Bar-Or <ybr719@gmail.com>
2026-06-07 18:28:51 +0300

    chore: remove stale Vite and scaffold folders

--- Workout/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import WorkoutTracker from './workout-tracker.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <WorkoutTracker />
-  </StrictMode>,
-)
